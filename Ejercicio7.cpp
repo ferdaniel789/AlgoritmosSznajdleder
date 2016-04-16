0a411a4c5c369f919cb4818b09cca4f081a9f73b
ferdaniel911 <ferdaniel911@localhost.localdomain>
2016-04-16 14:10:51 -0300

    CPP files added

---
+++ Ejercicio7.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Ejercicio7.cpp
+// Author      : Daniel Fernandez
+// Version     : v1
+// Copyright   : License-Free Software
+// Description : Trabajo practico
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	string a, b, c;
+	cout << "Ingrese año, mes y dia aleatorio" << endl;
+	cin >> a >> b >> c;
+
+	cout << a << b << c << endl;
+
+	return 0;
+}
---
+++ Ejercicio8.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : Ejercicio8.cpp
+// Author      : Daniel Fernandez
+// Version     : v1
+// Copyright   : License-Free Software
+// Description : Trabajo practico
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int fecha, anio, mes, dia, restoanio, restomes;
+	cout << "Ingrese fecha con formato aaaammdd (8 digitos)" << endl;
+	cin >> fecha;
+
+	anio = fecha / 10000;
+	restoanio = fecha % 10000;
+
+	mes = restoanio / 100;
+	restomes = restoanio % 100;
+
+	dia = restomes;
+
+	cout << "Dia: " << dia << " Mes: " << mes << " Año: " << anio << endl;
+
+
+
+	return 0;
+}
